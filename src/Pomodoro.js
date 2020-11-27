d0236f0fadb524acf9d2d1d765166a95e34c27e3
Jon White <jjwhite01@gmail.com>
2020-11-27 09:58:46 -0500

    adds dockerfile

--- src/Pomodoro.js
+++ src/Pomodoro.js
@@ -7,15 +7,15 @@ export default class Pomodoro extends Component {
     
     constructor(props){
         super(props);
-        const testPom = [
-            {description: "This is the first task", estimate: 3, completed: 0, done: false},
-            {description: "A second, easier task", estimate: 1, completed: 0, done: false},
-            {description: "Third task", estimate: 2, completed: 0, done: false},
-        ]
+        // const testPom = [
+        //     {description: "This is the first task", estimate: 3, completed: 0, done: false},
+        //     {description: "A second, easier task", estimate: 1, completed: 0, done: false},
+        //     {description: "Third task", estimate: 2, completed: 0, done: false},
+        // ]
 
         this.state = {
-            tasks: testPom,
-            notes: "Enter Notes"
+            tasks: [],
+            notes: ""
         };
     }
 
@@ -38,6 +38,21 @@ export default class Pomodoro extends Component {
         localStorage.setItem('pomodoro', JSON.stringify(this.state));
     }
 
+    addTask(){
+        let newTask = document.getElementById("new-pom").value;
+        let newEstimate = document.getElementById("new-estimate").value;
+        let tasks = this.state.tasks.slice();
+        tasks.push({
+            description: newTask,
+            estimate: Number(newEstimate),
+            completed: 0,
+            done: false
+        });
+        this.setState({tasks:tasks}, this.save);
+        document.getElementById("new-pom").value = "";
+        document.getElementById("new-estimate").value="1";
+    }
+
     render(){
         const todo = this.state.tasks.map((task, idx) => {
             return(
@@ -49,10 +64,12 @@ export default class Pomodoro extends Component {
             
                 <div className="poms">
                     {todo}
-                    <textarea id="pom-notes">
+                    <NewTask onClick={() => this.addTask()}/>
+                    <textarea placeholder="Enter notes here" id="pom-notes">
                       {this.state.notes}
                   </textarea>
                   <Save onClick={() => this.saveNotes()}/>
+                 
                 </div>
             
         )
@@ -66,5 +83,24 @@ function Save(props){
         <button className="btn btn-reset" onClick={props.onClick}>
             Save
         </button>
+    )
+}
+
+function NewTask(props){
+    return(
+        <div className="pomodoro-new">
+            <input type="text" placeholder="Enter a task description" id="new-pom"></input>
+            <select id="new-estimate">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+            </select>
+            <button className="btn btn-reset" onClick={props.onClick}>Add Task</button>
+        </div>
     )
 }
--- src/Timer.js
+++ src/Timer.js
@@ -1,5 +1,6 @@
 
 import React, { Component } from 'react'
+import Sound from 'react-sound'
 
 export default class Timer extends Component {
     
